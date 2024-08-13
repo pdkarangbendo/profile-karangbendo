0219eb2aaf00408f5a62c51ac4d4053d8b7b9406
pdkarangbendo <pdkarangbendo@gmail.com>
2024-08-13 09:23:52 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/(client)/page.tsx
@@ -0,0 +1,9 @@
+import ContainerClient from '@/components/containers/ContainerClient';
+
+export default function HomePage() {
+  return (
+    <ContainerClient>
+      <p>Ini halaman home</p>
+    </ContainerClient>
+  );
+}
